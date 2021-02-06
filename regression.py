295327c73e35875af43911b62bda1ecbb6a17b04
ashen007 <asheniranga@gmail.com>
2021-02-06 11:15:55 +0530

    getting cleaned data

--- regression.py
+++ regression.py
@@ -4,30 +4,29 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 
 def get_data():
-    data = pd.read_csv('./data.csv',usecols=['CRIM','ZN','INDUS','CHAS','NOX','AGE',
-                                             'DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV'], na_filter=True)
-    vif = pd.DataFrame()
-
-    # while True:
-    #     vif['features'] = data.columns
-    #     vif['vif value'] = [variance_inflation_factor(data.values,i) for i in range(data.shape[1])]
-    #     vif.sort_values(by='vif value',inplace=True)
-    #     vif.reset_index(inplace=True)
-    #     if all(vif['vfi values'] < 10):
-    #         break
-    #     else:
-    #         data.drop(vif.iloc[0]['features'],axis=1,inplace=True)
-
+    data = pd.read_csv('./data.csv', usecols=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'AGE',
+                                              'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV'], na_filter=True)
     return data
 
 
-data = get_data()
+def independent_data(data):
+    while True:
+        vfi = pd.DataFrame()
+        vfi['features'] = data.columns
+        vfi['vfi value'] = [variance_inflation_factor(data.values, i) for i in range(data.shape[1])]
+        vfi.sort_values(by='vfi value', inplace=True, ascending=False)
+        vfi.reset_index(inplace=True)
 
-vfi['features'] = data.columns
-vfi['vfi value'] = [variance_inflation_factor(data.values,i) for i in range(data.shape[1])]
-vfi.sort_values(by='vfi value',inplace=True,ascending=False)
+        if all(vfi['vfi value'] < 10):
+            # print('all independent')
+            break
+        else:
+            # print('dependent features')
+            # print(vfi)
+            # print(f"drop: {vfi.iloc[0]['features']}")
+            data.drop(vfi.iloc[0]['features'], axis=1, inplace=True)
+    return data
 
-if all(vfi['vfi value'] < 10):
-    print('all independent')
-else:
-    print('dependent features')
+
+df = independent_data(get_data())
+print(df)
