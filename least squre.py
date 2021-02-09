e5522b41570b127b520fc040db90dbcbefbc6537
ashen007 <asheniranga@gmail.com>
2021-02-09 13:32:08 +0530

    compare sklearn LinearRegression model with custom functions

--- least squre.py
+++ least squre.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score,mean_squared_error
 from matplotlib import pyplot as plt
 
 data = pd.read_csv('./data.csv',usecols=['CRIM','MEDV'])
@@ -14,8 +16,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 def mean_square(x, y):
     n = len(x)
-    coff_ = ((np.sum(y) * np.sum(x ** 2)) - (np.sum(x) * np.sum(x * y))) / (n * np.sum(x ** 2) - np.sum(x) ** 2)
-    inter_ = (n * np.sum(x * y) - np.sum(x) * np.sum(y)) / (n * np.sum(x ** 2) - np.sum(x) ** 2)
+    inter_ = ((np.sum(y) * np.sum(x ** 2)) - (np.sum(x) * np.sum(x * y))) / (n * np.sum(x ** 2) - np.sum(x) ** 2)
+    coff_ = (n * np.sum(x * y) - np.sum(x) * np.sum(y)) / (n * np.sum(x ** 2) - np.sum(x) ** 2)
 
     return coff_, inter_
 
@@ -25,15 +27,24 @@ def prediction(x, coefficient, intercept):
 
 
 if __name__ == '__main__':
+    X = np.array(x_train)
+    X = np.vstack([X,np.ones(len(X))]).T
+    Y = np.array(y_train)
     coefficient, intercept = mean_square(x_train, y_train)
+    coff,inter = np.linalg.lstsq(X,Y)[0]
+    predict_1 = prediction(x_test,coefficient,intercept)
+    predict_2 = prediction(x_test,coff,inter)
+    model = LinearRegression()
+    model.fit(x_train.reshape(-1,1),y_train)
+    predict_3 = model.predict(x_test.reshape(-1,1))
+
     plt.figure(figsize=[8,8],dpi=200)
-    sns.scatterplot(x_test,y_test)
-
-    for i in range(100):
-        coefficient,intercept = gradient_decent.gradient_decent(x_train,y_train,coefficient,intercept,0.001)
-        predictions = prediction(x_test,coefficient,intercept)
-        sns.lineplot(x_test,predictions,color='red')
-
-    plt.savefig('uni-varient.jpg')
+    sns.scatterplot(x_test,y_test,color='#0C154A')
+    sns.lineplot(x_test,predict_1,color='#F24405')
+    sns.lineplot(x_test,predict_2,color='#F21905')
+    plt.savefig('graphs/compare_two least squre functions.jpg')
     plt.show()
-    print(coefficient, intercept)
+
+    print(f'coefficient:{coefficient}, intercept:{intercept}, score:{r2_score(y_test,predict_1)*100}, mse:{mean_squared_error(y_test,predict_1)}')
+    print(f'coefficeint:{coff},intercept:{inter}, score:{r2_score(y_test,predict_2)*100}, mse:{mean_squared_error(y_test,predict_2)}')
+    print(f'coefficeint:{model.coef_},intercept:{model.intercept_}, score:{r2_score(y_test,predict_3)*100}, mse:{mean_squared_error(y_test,predict_3)}')
