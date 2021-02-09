1e822de8dae0eb1335082ad0cb2ff3f9a2b83621
ashen007 <asheniranga@gmail.com>
2021-02-09 15:06:59 +0530

    compare sklearn LinearRegression model with custom functions

--- least squre.py
+++ least squre.py
@@ -5,10 +5,10 @@ import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('./data.csv',usecols=['CRIM','MEDV'])
+data = pd.read_csv('./data.csv', usecols=['CRIM', 'MEDV'])
 x = np.array(data['CRIM'].values)
 y = np.array(data['MEDV'].values)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
@@ -28,23 +28,26 @@ def prediction(x, coefficient, intercept):
 
 if __name__ == '__main__':
     X = np.array(x_train)
-    X = np.vstack([X,np.ones(len(X))]).T
+    X = np.vstack([X, np.ones(len(X))]).T
     Y = np.array(y_train)
     coefficient, intercept = mean_square(x_train, y_train)
-    coff,inter = np.linalg.lstsq(X,Y)[0]
-    predict_1 = prediction(x_test,coefficient,intercept)
-    predict_2 = prediction(x_test,coff,inter)
+    coff, inter = np.linalg.lstsq(X, Y)[0]
+    predict_1 = prediction(x_test, coefficient, intercept)
+    predict_2 = prediction(x_test, coff, inter)
     model = LinearRegression()
-    model.fit(x_train.reshape(-1,1),y_train)
-    predict_3 = model.predict(x_test.reshape(-1,1))
+    model.fit(x_train.reshape(-1, 1), y_train)
+    predict_3 = model.predict(x_test.reshape(-1, 1))
 
-    plt.figure(figsize=[8,8],dpi=200)
-    sns.scatterplot(x_test,y_test,color='#0C154A')
-    sns.lineplot(x_test,predict_1,color='#F24405')
-    sns.lineplot(x_test,predict_2,color='#F21905')
+    plt.figure(figsize=[8, 8], dpi=200)
+    sns.scatterplot(x_test, y_test, color='#0C154A')
+    sns.lineplot(x_test, predict_1, color='#F24405')
+    sns.lineplot(x_test, predict_2, color='#F21905')
     plt.savefig('graphs/compare_two least squre functions.jpg')
     plt.show()
 
-    print(f'coefficient:{coefficient}, intercept:{intercept}, score:{r2_score(y_test,predict_1)*100}, mse:{mean_squared_error(y_test,predict_1)}')
-    print(f'coefficeint:{coff},intercept:{inter}, score:{r2_score(y_test,predict_2)*100}, mse:{mean_squared_error(y_test,predict_2)}')
-    print(f'coefficeint:{model.coef_},intercept:{model.intercept_}, score:{r2_score(y_test,predict_3)*100}, mse:{mean_squared_error(y_test,predict_3)}')
+    print(
+        f'coefficient:{coefficient}, intercept:{intercept}, score:{r2_score(y_test, predict_1) * 100}, mse:{mean_squared_error(y_test, predict_1)}')
+    print(
+        f'coefficeint:{coff},intercept:{inter}, score:{r2_score(y_test, predict_2) * 100}, mse:{mean_squared_error(y_test, predict_2)}')
+    print(
+        f'coefficeint:{model.coef_},intercept:{model.intercept_}, score:{r2_score(y_test, predict_3) * 100}, mse:{mean_squared_error(y_test, predict_3)}')
